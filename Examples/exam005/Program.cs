d5eb5cad4edd21a1d765fb586ad5ed8fce1ccd45
Avaz <qoraqosh1993@mail.ru>
2022-11-30 22:22:33 +0500

    add masha

---
+++ Examples/exam005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("vvedite imya polz ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("ura eto masha");
+}
+else
+{
+    Console.WriteLine("privet, ");
+    Console.WriteLine(username);
+}
